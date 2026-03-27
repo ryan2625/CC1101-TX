fde548e6728087b2b85c55753b0db5d2a2dd57e0
Ryan <ryancfreas@gmail.com>
2026-03-27 03:08:21 -0500

    Update main.cpp with more comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 /******************************************************************************
  * Author: Ryan Freas
- * Project: Interfacing your ESP32 and CC1101
+ * Date: 3/15/2026
+ * Project: ESP32-CC1101-Transmission
  *
- * Purpose:
+ * Description:
  * A program for transmitting a 315 MHz signal with the CC1101.
  * 
  ******************************************************************************/
@@ -21,31 +22,31 @@ extern "C" {
 }
 
 // Register values from the CC1101 datasheet  //
-// ================= STROBES ================ //
-constexpr uint8_t CC1101_STROBE_SRES      = 0x30;
-constexpr uint8_t CC1101_STROBE_STX       = 0x35;
-constexpr uint8_t CC1101_STROBE_SIDLE     = 0x36;
-constexpr uint8_t CC1101_STROBE_SFTX      = 0x3B;
-// ============ CONFIG REGISTERS ============ //
-constexpr uint8_t CC1101_CONFIG_FREQ2     = 0x0D;
+// ================= STROBES ================ //  // Strobe Description
+constexpr uint8_t CC1101_STROBE_SRES      = 0x30; // Reset Chip
+constexpr uint8_t CC1101_STROBE_STX       = 0x35; // Enter `TX` Mode
+constexpr uint8_t CC1101_STROBE_SIDLE     = 0x36; // Enter `IDLE` Mode
+constexpr uint8_t CC1101_STROBE_SFTX      = 0x3B; // Flush the TX FIFO
+// ============ CONFIG REGISTERS ============ //  // Field Name 
+constexpr uint8_t CC1101_CONFIG_FREQ2     = 0x0D; // FREQ[21:16]
 constexpr uint8_t CC1101_CONFIG_MDMCFG2   = 0x12; // SYNC_MODE, MOD_FORMAT
 constexpr uint8_t CC1101_CONFIG_DEVIATN   = 0x15; // DEVIATION_E, DEVIATION_M
 constexpr uint8_t CC1101_CONFIG_MDMCFG4   = 0x10; // DRATE_E
 constexpr uint8_t CC1101_CONFIG_MDMCFG3   = 0x11; // DRATE_M
-constexpr uint8_t CC1101_CONFIG_PATABLE   = 0x3E;
-constexpr uint8_t CC1101_CONFIG_SYNC1     = 0x04;
+constexpr uint8_t CC1101_CONFIG_PATABLE   = 0x3E; // PATABLE[0]
+constexpr uint8_t CC1101_CONFIG_SYNC1     = 0x04; // SYNC[15:8]
 constexpr uint8_t CC1101_CONFIG_MDMCFG1   = 0x13; // NUM_PREAMBLE
 constexpr uint8_t CC1101_CONFIG_PKTCTRL0  = 0x08; // LENGTH_CONFIG
-constexpr uint8_t CC1101_CONFIG_PKTLEN    = 0x06;
+constexpr uint8_t CC1101_CONFIG_PKTLEN    = 0x06; // PACKET_LENGTH
 constexpr uint8_t CC1101_CONFIG_IOCFG0    = 0x02; // GDO0_CFG
 constexpr uint8_t CC1101_CONFIG_FIFOTHR   = 0x03; // FIFO_THR
 constexpr uint8_t CC1101_CONFIG_MCSM1     = 0x17; // TXOFF_MODE
 constexpr uint8_t CC1101_CONFIG_MCSM0     = 0x18; // FS_AUTOCAL
 // ============= STATUS REGISTERS ============ //
-constexpr uint8_t CC1101_STATUS_TXBYTES   = 0x3A;
-constexpr uint8_t CC1101_STATUS_MARCSTATE = 0x35;
+constexpr uint8_t CC1101_STATUS_TXBYTES   = 0x3A; // NUM_TXBYTES
+constexpr uint8_t CC1101_STATUS_MARCSTATE = 0x35; // MARC_STATE[4:0]
 // =================== FIFO ================== //
-constexpr uint8_t CC1101_REG_FIFO         = 0x3F;
+constexpr uint8_t CC1101_REG_FIFO         = 0x3F; // TX FIFO
 // ================== VALUES ================= //
 constexpr uint8_t CC1101_VALUE_FREQ2      = 0x0C; // Frequency 315 MHz High Byte
 constexpr uint8_t CC1101_VALUE_FREQ1      = 0x1D; // Frequency 315 MHz Mid Byte
@@ -64,14 +65,14 @@ constexpr uint8_t CC1101_VALUE_IOCFG0     = 0x02; // GDO0 Config: FIFO Threshold
 constexpr uint8_t CC1101_VALUE_FIFOTHR    = 0x0E; // TX Threshold: 5 Bytes
 constexpr uint8_t CC1101_VALUE_MCSM1      = 0x31; // TXOFF Mode: FSTXON
 constexpr uint8_t CC1101_VALUE_MCSM0      = 0x14; // FS_AUTOCAL: Calibrate when IDLE -> TX
-constexpr uint8_t CC1101_DUMMY_BYTE       = 0x00; // Byte used to clock data on reads
+constexpr uint8_t CC1101_DUMMY_BYTE       = 0x00; // Byte Used for Reading Data (Full-Duplex Dummy Byte)
 // =========================================== //
 
 uint8_t calculate_header_byte(uint8_t address, bool read, bool burst) {
     /*
     The header byte is comprised of a R/W bit at position 7, burst bit at position 6, 
     and address bits at positions 5-0. We can simply OR the address bits by these
-    values to get the resulting header byte. See Assets/bit_shift.png for more info.
+    values below to get the resulting header byte. See Assets/bit_shift.png for more info.
 
     Example: TX FIFO address is 0011 1111 (0x3F) in binary. If we want burst access at this 
     address, we can OR it by a burst bit at position 6 which is 0100 0000 (0x40). 
@@ -83,7 +84,7 @@ uint8_t calculate_header_byte(uint8_t address, bool read, bool burst) {
 void spi_transaction(spi_device_handle_t cc1101, const uint8_t* data, size_t len,  const std::string& operation, bool shouldLog = false) {
     spi_transaction_t t = {};
     uint8_t rx[len];
-    t.tx_buffer = data;
+    t.tx_buffer = data; // tx[0] will always be the header byte
     t.rx_buffer = rx; // rx[0] will always be the Chip Status Byte (section 10.1 of the datasheet)
     t.length = len * 8;
     ESP_ERROR_CHECK(spi_device_polling_transmit(cc1101, &t));
@@ -350,6 +351,6 @@ extern "C" void app_main(void)
     );
 
     vTaskDelay(pdMS_TO_TICKS(1000));
-    log_after_tx(cc1101, 10); // Logs TXBYTES and MARCSTATE. Should now see TXFIFO_UNDERFLOW.
+    log_after_tx(cc1101, 10); // Logs TXBYTES and MARCSTATE, should now see TXFIFO_UNDERFLOW
     // ===================== END TRANSMISSION SECTION ======================= //
 }
